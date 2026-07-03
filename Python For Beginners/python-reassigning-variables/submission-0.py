193bf8533f6fa0d4b03be31ff1dc681762b02eb8
Dev Rajyaguru <devrajyaguru03@gmail.com>
2026-07-03 09:38:35 +0530

    Add: python-reassigning-variables - submission-0

---
+++ Python For Beginners/python-reassigning-variables/submission-0.py
@@ -0,0 +1,11 @@
+message_first = "The first message"
+
+print(message_first)
+
+message_second= "The second message"
+
+print(message_second)
+
+message_third = "The third message"
+
+print(message_third)
